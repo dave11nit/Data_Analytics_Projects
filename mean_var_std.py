ab6efbdb0b58dda4749c720069aaca9f5899bdaa
Devayan Mishra <66617952+dave11nit@users.noreply.github.com>
2021-09-17 20:52:08 +0530

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,5 +1,7 @@
 # Data analytics project for freecodecamp
 # mean_variance_stddeviation_calculator
+
+
 import numpy as np
 
 def calculate(list):
@@ -16,11 +18,16 @@ def calculate(list):
     # finding the values of variables
     mean = [list.mean(axis = 0).tolist(), list.mean(axis = 1).tolist(), np.mean(list).tolist()]
     # calculate the mean in a list and keep it in the list using tolist()
+    # calculate the mean in a list and keep it in the list using tolist()
+    # axis = 0 means mean of row elements 
+    # axis = 1 means mean of column elements 
+    # similarly did everything
+    
     variance = [list.var(axis = 0).tolist(), list.var(axis = 1).tolist(), np.var(list).tolist()]
     std_dev = [list.std(axis = 0).tolist(), list.var(axis = 1).tolist(), np.std(list).tolist()]
-    max = [list.max(axis = 0).tolist(), list.max(axis = 1).tolist(), np.max(list).tolist()]
-    min = [list.min(axis = 0).tolist(), list.min(axis = 1).tolist(), np.min(list).tolist()]
-    sum = [list.sum(axis = 0).tolist(), list.sum(axis = 1).tolist(), np.sum(list).tolist()]
+    maxi = [list.max(axis = 0).tolist(), list.max(axis = 1).tolist(), np.max(list).tolist()]
+    mini = [list.min(axis = 0).tolist(), list.min(axis = 1).tolist(), np.min(list).tolist()]
+    sum1 = [list.sum(axis = 0).tolist(), list.sum(axis = 1).tolist(), np.sum(list).tolist()]
     # created all the variable
 
     # return a dictionary of all the above variables
@@ -28,9 +35,9 @@ def calculate(list):
         'mean':mean,
         'variance':variance,
         'standard_deviation':std_dev,
-        'max':max,
-        'min':min,
-        'sum':sum
+        'max':maxi,
+        'min':mini,
+        'sum':sum1
     }
 
 print(calculate([1,2,3,4,5,6,7,8,9]))
